468d9b08562a1add4003ca890d867ad5ec2867cd
Maxwell Croy <max.croy1@gmail.com>
2020-05-22 21:21:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,5 +2,5 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
   strings_array = Array.new 
-  
+  for i in 0...(src.length)
 end
